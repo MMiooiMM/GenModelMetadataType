de743f4ef69b18953d163dbc38dac6291404bb2e
黃郁倫 mio <mio@miniasp.com>
2020-12-07 17:42:56 +0800

    取得 DLL 範本

--- GenModelMetadataType/Program.cs
+++ GenModelMetadataType/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace GenModelMetadataType
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            // 找到 DLL
+            var assembly = GetAssembly(@"..\..\..\..\..\ASPNETCore5ModelMetadataType\WebApplication4\bin\Debug\net5.0", "WebApplication4");
+
+            // 讀取 DLL
+
+            // 產生內容
+
             Console.WriteLine("Hello World!");
+        }
+
+        private static Assembly GetAssembly(string path, string name)
+        {
+            string localPath = string.IsNullOrEmpty(path) ? Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) : path;
+            string assemblyFilePath = Path.Combine(localPath, $"{name}.dll");
+                        
+            if (!File.Exists(assemblyFilePath))
+            {
+                throw new Exception("Missing " + assemblyFilePath);
+            }            
+
+            return Assembly.LoadFrom(assemblyFilePath);
         }
     }
 }
