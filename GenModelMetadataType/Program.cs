1df5587a75d66c20d66d34c632630ae72b05ea51
黃郁倫 mio <mio@miniasp.com>
2020-12-07 18:40:00 +0800

    透過錯誤取得 DbContext 類型

--- GenModelMetadataType/Program.cs
+++ GenModelMetadataType/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace GenModelMetadataType
@@ -12,6 +13,7 @@ namespace GenModelMetadataType
             var assembly = GetAssembly(@"..\..\..\..\..\ASPNETCore5ModelMetadataType\WebApplication4\bin\Debug\net5.0", "WebApplication4");
 
             // 讀取 DLL
+            ReadAssembly(assembly);
 
             // 產生內容
 
@@ -22,13 +24,34 @@ namespace GenModelMetadataType
         {
             string localPath = string.IsNullOrEmpty(path) ? Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) : path;
             string assemblyFilePath = Path.Combine(localPath, $"{name}.dll");
-                        
+
             if (!File.Exists(assemblyFilePath))
             {
-                throw new Exception("Missing " + assemblyFilePath);
-            }            
+                throw new Exception($"Missing {assemblyFilePath}");
+            }
 
             return Assembly.LoadFrom(assemblyFilePath);
+        }
+
+        private static void ReadAssembly(Assembly assembly)
+        {
+            string deContextNamespace = "Microsoft.EntityFrameworkCore.DbContext";
+            Type dbContextType;
+
+            try
+            {
+                dbContextType = assembly.GetTypes().FirstOrDefault();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                dbContextType = e.Types
+                    .FirstOrDefault(t => t.BaseType.FullName.Contains(deContextNamespace) && t.IsPublic);
+            }
+
+            if(dbContextType is null)
+            {
+                throw new Exception("Missing {deContextNamespace}");
+            }            
         }
     }
 }
