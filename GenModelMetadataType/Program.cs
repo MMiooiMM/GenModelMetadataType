db162b3313c66f3706f6efcd9325440821075571
黃郁倫 mio <mio@miniasp.com>
2020-12-08 11:41:35 +0800

    在程式啟動目錄建立 Partial Code 檔案

--- GenModelMetadataType/Program.cs
+++ GenModelMetadataType/Program.cs
@@ -9,16 +9,34 @@ namespace GenModelMetadataType
 {
     internal class Program
     {
+        private static readonly Dictionary<Type, string> typeAlias = new Dictionary<Type, string>
+        {
+            { typeof(bool), "bool" },
+            { typeof(byte), "byte" },
+            { typeof(char), "char" },
+            { typeof(decimal), "decimal" },
+            { typeof(double), "double" },
+            { typeof(float), "float" },
+            { typeof(int), "int" },
+            { typeof(long), "long" },
+            { typeof(object), "object" },
+            { typeof(sbyte), "sbyte" },
+            { typeof(short), "short" },
+            { typeof(string), "string" },
+            { typeof(uint), "uint" },
+            { typeof(ulong), "ulong" },
+            { typeof(void), "void" }
+        };
+
         private static void Main(string[] args)
         {
-            // 找到 DLL
             var assembly = GetAssembly(@"..\..\..\..\..\ASPNETCore5ModelMetadataType\WebApplication4\bin\Debug\net5.0", "WebApplication4");
 
-            // 讀取 DLL 並取得 entity
             var types = GetEntityTypesFromAssembly(assembly);
 
-            // 產生內容
-            Console.WriteLine("Hello World!");
+            CreateFiles(types);
+
+            Console.WriteLine("Done.");
         }
 
         /// <summary>
@@ -102,6 +120,74 @@ namespace GenModelMetadataType
             sb.Append(">");
 
             return sb.ToString();
-        }       
+        }
+
+        /// <summary>
+        /// 新增檔案
+        /// </summary>
+        /// <param name="types"></param>
+        private static void CreateFiles(IEnumerable<Type> types)
+        {
+            foreach (var type in types)
+            {
+                var fileName = $"{type.Name}.Partial.cs";
+                var fileContent = GeneratePartialCodeContent(type);
+
+                using StreamWriter sw = new StreamWriter($"{Environment.CurrentDirectory}\\{fileName}");
+                sw.Write(fileContent);
+            }
+        }
+
+        /// <summary>
+        /// 產生 Partial Code 內容
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static string GeneratePartialCodeContent(Type type)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("using Microsoft.AspNetCore.Mvc;");
+            sb.AppendLine("using System;");
+            sb.AppendLine("using System.Collections.Generic;");
+            sb.AppendLine("using System.ComponentModel.DataAnnotations;");
+            sb.AppendLine();
+            sb.AppendLine("#nullable disable");
+            sb.AppendLine();
+            sb.AppendLine($"namespace {type.Namespace}");
+            sb.AppendLine("{");
+            sb.AppendLine($"    [ModelMetadataType(typeof({type.Name}Metadata))]");
+            sb.AppendLine($"    public partial class {type.Name}");
+            sb.AppendLine("    {");
+            sb.AppendLine("    }");
+            sb.AppendLine();
+            sb.AppendLine($"    internal class {type.Name}Metadata");
+            sb.AppendLine("    {");
+
+            foreach (var prop in type.GetProperties())
+            {
+                if (prop.GetGetMethod().IsVirtual)
+                {
+                    continue;
+                }
+                sb.AppendLine("        // [Required]");
+                sb.AppendLine($"        public {GetTypeAliasOrName(prop.PropertyType)} {prop.Name} {{ get; set; }}");
+            }
+
+            sb.AppendLine("    }");
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 取得原始型別的別名
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        private static string GetTypeAliasOrName(Type type)
+        {
+            return typeAlias.TryGetValue(type, out string alias) ? alias : type.Name;
+        }
     }
 }
