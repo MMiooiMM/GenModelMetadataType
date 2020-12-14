d43ac2d629a64a87e8be381df9dcc9a8cdbbb7cc
黃郁倫 mio <mio@miniasp.com>
2020-12-14 10:17:05 +0800

    最低紀錄等級必須設定為 LogLevel.Warning 以上

--- GenModelMetadataType/Program.cs
+++ GenModelMetadataType/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
 namespace GenModelMetadataType
 {
     internal class Program
@@ -12,17 +11,38 @@ namespace GenModelMetadataType
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<App>();
                 })
+                .ConfigureAppConfiguration(delegate (HostBuilderContext hostingContext, IConfigurationBuilder config)
+                {
+                    config.Sources.Clear();
+                    //IHostEnvironment hostingEnvironment = hostingContext.HostingEnvironment;
+                    //bool value = hostingContext.Configuration.GetValue("hostBuilder:reloadConfigOnChange", defaultValue: true);
+                    //config.AddJsonFile("appsettings.json", optional: true, value).AddJsonFile("appsettings." + hostingEnvironment.EnvironmentName + ".json", optional: true, value);
+                    //if (hostingEnvironment.IsDevelopment() && !string.IsNullOrEmpty(hostingEnvironment.ApplicationName))
+                    //{
+                    //    Assembly assembly = Assembly.Load(new AssemblyName(hostingEnvironment.ApplicationName));
+                    //    if (assembly != null)
+                    //    {
+                    //        config.AddUserSecrets(assembly, optional: true);
+                    //    }
+                    //}
+                    //config.AddEnvironmentVariables();
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
                 .ConfigureLogging((hostContext, configLogging) =>
                 {
-                    configLogging.AddConsole();
-                    configLogging.AddDebug();
+                    //configLogging.ClearProviders();
+                    //configLogging.AddConsole();
+                    //configLogging.AddDebug();
+                    configLogging.SetMinimumLevel(LogLevel.Warning);
                 });
     }
 }
